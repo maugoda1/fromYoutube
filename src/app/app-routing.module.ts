755e60b93cb68184bc39a05c3b525b7385b43f2e
maugoda1 <jhongomezda@gmail.com>
2018-02-17 12:48:09 -0500

    Componente user iniciado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from "@angular/core";
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/appComponent', pathMatch: 'full' },  
+  { path: 'appComponent', component: AppComponent }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
